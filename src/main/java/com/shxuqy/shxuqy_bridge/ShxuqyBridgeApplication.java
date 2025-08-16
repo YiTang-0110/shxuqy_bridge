9694c6857bd3917b8c7e46bf26e1fbeea33ee266
2501_90863906 <proxcy@163.com>
2025-08-16 09:09:38 +0800

    1

---
+++ src/main/java/com/shxuqy/shxuqy_bridge/ShxuqyBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.shxuqy.shxuqy_bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShxuqyBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShxuqyBridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shxuqy/shxuqy_bridge/ShxuqyBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shxuqy.shxuqy_bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShxuqyBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
